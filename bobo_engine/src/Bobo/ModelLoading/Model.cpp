278a42ee5835bf066e3fe5d8c8627bcaf5f930a4
Amber Ma <agmlammy@gmail.com>
2023-09-15 20:26:47 -0700

    added code to ModelData.cpp's destructor (the variables will terminate after program terminates)

--- bobo_engine/src/Bobo/ModelLoading/Model.cpp
+++ bobo_engine/src/Bobo/ModelLoading/Model.cpp
@@ -3,11 +3,15 @@
 
 namespace Bobo
 {
+	/** Model constructor that takes in model data object for use
+	* once loaded into model loader.
+	*/
 	Model::Model(ModelData* modelData)
 	{
 		modelData = modelData;
 	}
 
+	/** Model destructor. */
 	Model::~Model()
 	{
 
--- bobo_engine/src/Bobo/ModelLoading/ModelData.cpp
+++ bobo_engine/src/Bobo/ModelLoading/ModelData.cpp
@@ -3,6 +3,13 @@
 
 namespace Bobo
 {
+	/** Model Data Constructor that takes in vertices, uv, and normals values in order to 
+	store model data for rendering. 
+	@param vertices
+	@param uvs
+	@param normals
+	*/
+
 	ModelData::ModelData(std::vector<glm::vec3> vertices, std::vector<glm::vec2> uvs, std::vector<glm::vec3> normals)
 	{
 		m_Vertices = vertices;
@@ -10,8 +17,13 @@ namespace Bobo
 		m_Normals = normals;
 	}
 
+	/* Model data destructor for use upon program termination.
+	*/
+
 	ModelData::~ModelData()
 	{
-
+		delete m_Vertices;
+		delete m_UVs;
+		delete m_Normals;
 	}
 }
