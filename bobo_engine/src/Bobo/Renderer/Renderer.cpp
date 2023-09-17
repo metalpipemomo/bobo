0094aee03f953fdf73fb88f9eb21e69b60203073
jkcadee <jkwok55@my.bcit.ca>
2023-09-17 11:38:53 -0700

    added comments to plan out how to implement rendering

--- bobo_engine/src/Bobo/Renderer/Renderer.cpp
+++ bobo_engine/src/Bobo/Renderer/Renderer.cpp
@@ -1,6 +1,31 @@
 #include "bobopch.h";
+#include "Renderer.h";
 
-class Renderer
+namespace bobo
 {
-	// cool :)
-};
+	Renderer::Renderer()
+	{
+		// list of models
+	}
+
+	Renderer::~Renderer()
+	{
+
+	}
+
+	Renderer::DrawVertices()
+	{
+		// the model/entity should have a list of vertex points, we want to take these and put them into a vertex buffer object.
+		// we then bind the buffer
+		// afterwards we take that buffer data and draw the vertex points.
+		// should be returning the buffer
+	}
+
+	Renderer::DrawNormals()
+	{
+		// same thing as vertices but we are dealing with the normals of the model instead
+	}
+
+	// we probably need colours too
+
+}
--- bobo_engine/src/Bobo/Renderer/Renderer.h
+++ bobo_engine/src/Bobo/Renderer/Renderer.h
@@ -1,1 +1,17 @@
 #pragma once
+
+namespace bobo
+{
+	class Renderer
+	{
+	public:
+		Renderer();
+		virtual ~Renderer();
+		// eventually we want to be taking from an entity obj but right now lets take a model and render it
+		DrawVertices();
+		DrawNormals();
+		
+	private:
+		
+	};
+}
