8783d8c05cc73aee3ba1676b4236492c5bdb41e9
Mark Keeble <markkeeble14@gmail.com>
2023-11-05 15:48:34 -0800

    fixes to make build error go away. Made gen.bat copy the relevant files to imgui folder and made premake5 build the solution ensuring that the imgui.cpp files do not require us including the precompiled header

--- gen.bat
+++ gen.bat
@@ -3,5 +3,10 @@ call xcopy "bobo\libs\FMODAPI\api\core\lib\x64\fmod.dll" "bin\Debug-windows-x86_
 call xcopy "bobo\libs\FMODAPI\api\studio\lib\x64\fmodstudio.dll" "bin\Debug-windows-x86_64\bobo\" /y
 call xcopy "bobo\libs\FMODAPI\api\core\lib\x64\fmod.dll" "bin\Release-windows-x86_64\bobo\" /y
 call xcopy "bobo\libs\FMODAPI\api\studio\lib\x64\fmodstudio.dll" "bin\Release-windows-x86_64\bobo\" /y
+call xcopy "bobo\libs\imgui\backends\imgui_impl_glfw.cpp" "bobo\libs\imgui\" /y
+call xcopy "bobo\libs\imgui\backends\imgui_impl_glfw.h" "bobo\libs\imgui\" /y
+call xcopy "bobo\libs\imgui\backends\imgui_impl_opengl3.h" "bobo\libs\imgui\" /y
+call xcopy "bobo\libs\imgui\backends\imgui_impl_opengl3.cpp" "bobo\libs\imgui\" /y
+call xcopy "bobo\libs\imgui\backends\imgui_impl_opengl3_loader.h" "bobo\libs\imgui\" /y
 call premake\premake5.exe vs2022
 PAUSE
