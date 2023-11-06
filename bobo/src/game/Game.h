21edb6ebde4d8aff7459c0273d1c91d0e465457f
jmajam <54956224+jmajam@users.noreply.github.com>
2023-11-06 15:10:07 -0800

    Added ball model and relevant textures (#28)
    * Added ball model and relevant textures
    
    * show the ball loads
    
    textures seem to not load correctly

--- bobo/src/game/Game.h
+++ bobo/src/game/Game.h
@@ -26,7 +26,7 @@ private:
 
 		// Get the 'Transform' component from GameObject
 		auto transform = object->GetComponent<Transform>();
-		object->AddComponent<Material>(ModelLoader::GetModel("cube"), TextureLoader::GetTexture("kar"));
+		object->AddComponent<Material>(ModelLoader::GetModel("Ball_1"), TextureLoader::GetTexture("1"));
 
 		// Log initial transform position values
 		Log("Initial position x: {}, y: {}, z: {}",
