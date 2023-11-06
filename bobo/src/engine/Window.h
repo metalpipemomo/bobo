7c56c4ad5be43a0aebb70fc80a1258ae0223a928
Anna Spray-Wiegert <144171254+vytalrose@users.noreply.github.com>
2023-11-06 15:24:12 -0800

    created demo ui
    simple docked window with some text demonstrating how many balls are remaining to be sunk

--- bobo/src/engine/Window.h
+++ bobo/src/engine/Window.h
@@ -71,10 +71,8 @@ public:
 		while (!glfwWindowShouldClose(p_Window))
 		{
 			// ImGui Frame Updates
-			ImGui_ImplOpenGL3_NewFrame();
-			ImGui_ImplGlfw_NewFrame();
-			ImGui::NewFrame();
-			ImGui::ShowDemoWindow(); // TODO Remove this line when starting work on our own UI
+			CreateImGuiForGame();
+			//ImGui::ShowDemoWindow(); // TODO Remove this line when starting work on our own UI
 
 			// System Frame Updates
 			Renderer::Update();
@@ -177,6 +175,45 @@ private:
 		// Initialize Platform and Rendering backends
 		ImGui_ImplGlfw_InitForOpenGL(p_Window, true);
 		ImGui_ImplOpenGL3_Init();
+	}
+
+	void CreateImGuiForGame() {
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		static bool no_titlebar = false;
+		static bool no_scrollbar = true;
+		static bool no_menu = true;
+		static bool no_move = true;
+		static bool no_resize = true;
+		static bool no_collapse = true;
+		static bool no_close = true;
+		static bool no_nav = true;
+		static bool no_background = false;
+		static bool no_bring_to_front = false;
+		static bool unsaved_document = false;
+
+		ImGuiWindowFlags window_flags = 0;
+		if (no_titlebar)        window_flags |= ImGuiWindowFlags_NoTitleBar;
+		if (no_scrollbar)       window_flags |= ImGuiWindowFlags_NoScrollbar;
+		if (!no_menu)           window_flags |= ImGuiWindowFlags_MenuBar;
+		if (no_move)            window_flags |= ImGuiWindowFlags_NoMove;
+		if (no_resize)          window_flags |= ImGuiWindowFlags_NoResize;
+		if (no_collapse)        window_flags |= ImGuiWindowFlags_NoCollapse;
+		if (no_nav)             window_flags |= ImGuiWindowFlags_NoNav;
+		if (no_background)      window_flags |= ImGuiWindowFlags_NoBackground;
+		if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+		if (unsaved_document)   window_flags |= ImGuiWindowFlags_UnsavedDocument;
+
+		const ImGuiViewport* main_viewport = ImGui::GetMainViewport();
+		ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x + 100, main_viewport->WorkPos.y + 20), 0);
+		ImGui::SetNextWindowSize(ImVec2(440, 70), 0);
+
+		ImGui::Begin("Score", NULL, window_flags);
+		ImGui::Text("Solid Balls Remaining: 7");
+		ImGui::Text("Striped Balls Remaining: 7");
+		ImGui::End();
 	}
 
 	void Update()
