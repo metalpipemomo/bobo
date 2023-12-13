935ba0de1eddc584b52af29ad712d559a2f958f0
Johnny <johnny@scottmail.ca>
2023-12-12 20:10:35 -0800

    commented rigidbody and physics files

--- src/engine/Physics/Physics.h
+++ src/engine/Physics/Physics.h
@@ -1,5 +1,4 @@
 #pragma once
-// going to need an update function
 #include <Jolt/Jolt.h>
 
 
@@ -18,16 +17,15 @@
 #include "../EntityComponent/EntityManager.h"
 #include "../Time.h"
 
-// Disable common warnings triggered by Jolt, you can use JPH_SUPPRESS_WARNING_PUSH / JPH_SUPPRESS_WARNING_POP to store and restore the warning state
 JPH_SUPPRESS_WARNINGS
 
-// All Jolt symbols are in the JPH namespace
 using namespace JPH;
 
+/// **** All of this below here is jolt system set up variables required to create a base physics system with jolt,
+//  **** game dev people do not concern yourselves.
+
+
 // Layer that objects can be in, determines which other objects it can collide with
-// Typically you at least want to have 1 layer for moving bodies and 1 layer for static bodies, but you can have more
-// layers if you want. E.g. you could have a layer for high detail collision (which is not used by the physics simulation
-// but only if you do collision testing).
 namespace Layers
 {
 	static constexpr JPH::ObjectLayer NON_MOVING = 0;
@@ -55,11 +53,8 @@ public:
 };
 
 
-// Each broadphase layer results in a separate bounding volume tree in the broad phase. You at least want to have
-// a layer for non-moving and moving objects to avoid having to update a tree full of static objects every frame.
-// You can have a 1-on-1 mapping between object layers and broadphase layers (like in this case) but if you have
-// many object layers you'll be creating many broad phase trees, which is not efficient. If you want to fine tune
-// your broadphase layers define JPH_TRACK_BROADPHASE_STATS and look at the stats reported on the TTY.
+// Broadphase layer distinctions, differentiating between non_moving and moving prevents 
+// the bounding volume tree to have to update static objects every frame.
 namespace BroadPhaseLayers
 {
 	static constexpr JPH::BroadPhaseLayer NON_MOVING(0);
@@ -68,13 +63,12 @@ namespace BroadPhaseLayers
 };
 
 // BroadPhaseLayerInterface implementation
-// This defines a mapping between object and broadphase layers.
+// defines a mapping between object and broadphase layers.
 class BPLayerInterfaceImpl final : public BroadPhaseLayerInterface
 {
 public:
 	BPLayerInterfaceImpl()
 	{
-		// Create a mapping table from object to broad phase layer
 		mObjectToBroadPhase[Layers::NON_MOVING] = BroadPhaseLayers::NON_MOVING;
 		mObjectToBroadPhase[Layers::MOVING] = BroadPhaseLayers::MOVING;
 	}
@@ -125,14 +119,14 @@ public:
 	}
 };
 
-// An example contact listener
+// Contact manager that handles events when physics objects come into contact with one another
 class MyContactListener : public JPH::ContactListener
 {
 public:
 	// See: ContactListener
 	virtual JPH::ValidateResult OnContactValidate(const JPH::Body& inBody1, const JPH::Body& inBody2, JPH::RVec3Arg inBaseOffset, const JPH::CollideShapeResult& inCollisionResult) override
 	{
-		std::cout << "Contact validate callback" << std::endl;
+		//std::cout << "Contact validate callback" << std::endl;
 
 		// Allows you to ignore a contact before it is created (using layers to not make objects collide is cheaper!)
 		return JPH::ValidateResult::AcceptAllContactsForThisBodyPair;
@@ -140,17 +134,17 @@ public:
 
 	virtual void OnContactAdded(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings) override
 	{
-		std::cout << "A contact was added" << std::endl;
+		//std::cout << "A contact was added" << std::endl;
 	}
 
 	virtual void OnContactPersisted(const JPH::Body& inBody1, const JPH::Body& inBody2, const JPH::ContactManifold& inManifold, JPH::ContactSettings& ioSettings) override
 	{
-		std::cout << "A contact was persisted" << std::endl;
+		//std::cout << "A contact was persisted" << std::endl;
 	}
 
 	virtual void OnContactRemoved(const JPH::SubShapeIDPair& inSubShapePair) override
 	{
-		std::cout << "A contact was removed" << std::endl;
+		//std::cout << "A contact was removed" << std::endl;
 	}
 };
 
@@ -225,24 +219,9 @@ private:
 	std::map<BodyID, function<void(BodyID)>> onCollisionPersistListeners;
 };
 
-// An example activation listener
-class MyBodyActivationListener : public JPH::BodyActivationListener
-{
-public:
-	virtual void OnBodyActivated(const JPH::BodyID& inBodyID, JPH::uint64 inBodyUserData) override
-	{
-		// 
-	}
-
-	virtual void OnBodyDeactivated(const JPH::BodyID& inBodyID, JPH::uint64 inBodyUserData) override
-	{
-		//
-	}
-};
+// **** End of Jolt Set up variables.
 
 
-
-// STL includes
 class Physics
 {
 public:
@@ -262,22 +241,14 @@ public:
 	
 	static void Update() 
 	{
+		// Allocates block of memory for physics
 		TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
 
-		// We need a job system that will execute physics jobs on multiple threads. Typically
-		// you would implement the JobSystem interface yourself and let Jolt Physics run on top
-		// of your own job scheduler. JobSystemThreadPool is an example implementation.
+		// Base Job Handler system. why do these things have to be constantly created? it breaks if i try and make it a static variable
 		JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1);
-		auto &body_interface = Physics::PhysicsWorld->physics_system->GetBodyInterface();
-		// Output current position and velocity of the sphere
-		//RVec3 position = body_interface.GetCenterOfMassPosition(rbid);
-		//Vec3 velocity = body_interface.GetLinearVelocity(rbid);
-		//std::cout << "Step " << 0 << ": Position = (" << position.GetX() << ", " << position.GetY() << ", " << position.GetZ() << "), Velocity = (" << velocity.GetX() << ", " << velocity.GetY() << ", " << velocity.GetZ() << ")" << std::endl;
-
-		// If you take larger steps than 1 / 60th of a second you need to do multiple collision steps in order to keep the simulation stable. Do 1 collision step per 1 / 60th of a second (round up).
+	
 		const int cCollisionSteps = 1;
 
-		
 		// Step the world
 		Physics::PhysicsWorld->physics_system->Update((Time::GetTimeScale()/60.0), cCollisionSteps, &temp_allocator, &job_system);
 	}
@@ -290,13 +261,11 @@ public:
 	void SetJoltRbEntity(BodyID id, Entity entity) {
 		ecsJoltBodyHolder[id] = entity;
 	}
-
+	
 	ActionableContactListener *m_ContactListener;
-
 private:
 	Physics() 
 	{
-		// johnnys stuff/ /
     	// Register allocation hook
 		RegisterDefaultAllocator();
 
@@ -307,61 +276,32 @@ private:
 		// Register all Jolt physics types
 		RegisterTypes();
 
-		// We need a temp allocator for temporary allocations during the physics update. We're
-		// pre-allocating 10 MB to avoid having to do allocations during the physics update.
-		// B.t.w. 10 MB is way too much for this example but it is a typical value you can use.
-		// If you don't want to pre-allocate you can also use TempAllocatorMalloc to fall back to
-		// malloc / free.
-		TempAllocatorImpl temp_allocator(10 * 1024 * 1024);
-
-		// We need a job system that will execute physics jobs on multiple threads. Typically
-		// you would implement the JobSystem interface yourself and let Jolt Physics run on top
-		// of your own job scheduler. JobSystemThreadPool is an example implementation.
-		JobSystemThreadPool job_system(cMaxPhysicsJobs, cMaxPhysicsBarriers, thread::hardware_concurrency() - 1);
-
 		// This is the max amount of rigid bodies that you can add to the physics system. If you try to add more you'll get an error.
-		// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
 		const uint cMaxBodies = 1024;
 
 		// This determines how many mutexes to allocate to protect rigid bodies from concurrent access. Set it to 0 for the default settings.
 		const uint cNumBodyMutexes = 0;
 
 		// This is the max amount of body pairs that can be queued at any time (the broad phase will detect overlapping
-		// body pairs based on their bounding boxes and will insert them into a queue for the narrowphase). If you make this buffer
-		// too small the queue will fill up and the broad phase jobs will start to do narrow phase work. This is slightly less efficient.
-		// Note: This value is low because this is a simple test. For a real project use something in the order of 65536.
+		// body pairs based on their bounding boxes and will insert them into a queue for the narrowphase)
 		const uint cMaxBodyPairs = 1024;
 
-		// This is the maximum size of the contact constraint buffer. If more contacts (collisions between bodies) are detected than this
-		// number then these contacts will be ignored and bodies will start interpenetrating / fall through the world.
-		// Note: This value is low because this is a simple test. For a real project use something in the order of 10240.
+		// This is the maximum size of the contact constraint buffer. 
 		const uint cMaxContactConstraints = 1024;
 
 		// Create mapping table from object layer to broadphase layer
-		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
 		broad_phase_layer_interface = new BPLayerInterfaceImpl{};
 
 		// Create class that filters object vs broadphase layers
-		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
 		object_vs_broadphase_layer_filter = new ObjectVsBroadPhaseLayerFilterImpl{};
 
 		// Create class that filters object vs object layers
-		// Note: As this is an interface, PhysicsSystem will take a reference to this so this instance needs to stay alive!
 		object_vs_object_layer_filter = new ObjectLayerPairFilterImpl{};
 
-		// Now we can create the actual physics system.
 		physics_system = new PhysicsSystem{};
 		physics_system->Init(cMaxBodies, cNumBodyMutexes, cMaxBodyPairs, cMaxContactConstraints, *broad_phase_layer_interface, *object_vs_broadphase_layer_filter, *object_vs_object_layer_filter);
 
-		// A body activation listener gets notified when bodies activate and go to sleep
-		// Note that this is called from a job so whatever you do here needs to be thread safe.
-		// Registering one is entirely optional.
-		body_activation_listener = new MyBodyActivationListener{};
-		physics_system->SetBodyActivationListener(body_activation_listener);
 
-		// A contact listener gets notified when bodies (are about to) collide, and when they separate again.
-		// Note that this is called from a job so whatever you do here needs to be thread safe.
-		// Registering one is entirely optional.
 
 		// this contact listener is what lets rigidbodies have their own collision-based events
 		m_ContactListener = new ActionableContactListener();
@@ -375,12 +315,6 @@ private:
 	ObjectVsBroadPhaseLayerFilterImpl *object_vs_broadphase_layer_filter;
 	ObjectLayerPairFilterImpl *object_vs_object_layer_filter;
 	BPLayerInterfaceImpl *broad_phase_layer_interface;
-
-	// We need a job system that will execute physics jobs on multiple threads. Typically
-	// you would implement the JobSystem interface yourself and let Jolt Physics run on top
-	// of your own job scheduler. JobSystemThreadPool is an example implementation.
-	//listeners for when things collide? These are apparently entirely optional 
-	MyBodyActivationListener *body_activation_listener;
 
 	std::map<BodyID, Entity> ecsJoltBodyHolder;
 };
